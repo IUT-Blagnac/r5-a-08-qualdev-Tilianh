65d7a5870086e41cf023cc4d471cf2164aa1337c
Tilian-HURE <tilian.contact@gmail.com>
2023-12-11 12:02:04 +0100

    TP#1

---
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,29 @@
+package hellocucumber;
+
+
+import io.cucumber.java.en.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class StepDefinitions {
+
+    String today = "lundi";
+    String answer = "Amongus";
+
+    @Given("on est {string}")
+    public void todayIs(String day) {
+        this.today = day;
+    }
+
+    @When("on demande si on est vendredi")
+    public void askIfItsVendredi() {
+        this.answer = this.today.equals("vendredi") ? "Oui" : "Non";
+    }
+
+    @Then("répondre {string}")
+    public void answer(String message) {
+        assertEquals(message, answer);
+    }
+
+}
